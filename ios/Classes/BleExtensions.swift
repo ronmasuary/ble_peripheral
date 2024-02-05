5a97cc1dfb6c8f064881f7c419ff6c0dfddd3686
levi <levi@wikey.ai>
2024-02-05 12:26:37 +0200

    remove ios support for ble data advertising as it currently not supported

--- ios/Classes/BleExtensions.swift
+++ ios/Classes/BleExtensions.swift
@@ -12,7 +12,7 @@
 #else
     #error("Unsupported platform.")
 #endif
-import CoreBluetooth
+// import CoreBluetooth
 import Foundation
 
 enum CustomError: Error {
@@ -20,135 +20,135 @@ enum CustomError: Error {
 }
 
 /// local list of characteristic
-var characteristicsList = [CBMutableCharacteristic]()
+// var characteristicsList = [CBMutableCharacteristic]()
 
-extension CBService {
-    func toBleService() -> BleService {
-        return BleService(
-            uuid: uuid.toUUID(),
-            primary: isPrimary,
-            characteristics: characteristics?.map { char in
-                char.toBleCharacteristic()
-            } ?? []
-        )
-    }
-}
+// extension CBService {
+//     func toBleService() -> BleService {
+//         return BleService(
+//             uuid: uuid.toUUID(),
+//             primary: isPrimary,
+//             characteristics: characteristics?.map { char in
+//                 char.toBleCharacteristic()
+//             } ?? []
+//         )
+//     }
+// }
 
-extension BleService {
-    func toCBService() -> CBMutableService {
-        let service = CBMutableService(
-            type: CBUUID(string: uuid.value),
-            primary: primary
-        )
-        let chars = characteristics.compactMap { bleChar in
-            bleChar?.toCBChar()
-        }
-        if !chars.isEmpty {
-            service.characteristics = chars
-        }
-        return service
-    }
-}
+// extension BleService {
+//     func toCBService() -> CBMutableService {
+//         let service = CBMutableService(
+//             type: CBUUID(string: uuid.value),
+//             primary: primary
+//         )
+//         let chars = characteristics.compactMap { bleChar in
+//             bleChar?.toCBChar()
+//         }
+//         if !chars.isEmpty {
+//             service.characteristics = chars
+//         }
+//         return service
+//     }
+// }
 
-extension BleDescriptor {
-    func toCBMutableDescriptor() -> CBMutableDescriptor {
-        return CBMutableDescriptor(type: CBUUID(string: uuid.value), value: value?.toData())
-    }
-}
+// extension BleDescriptor {
+//     func toCBMutableDescriptor() -> CBMutableDescriptor {
+//         return CBMutableDescriptor(type: CBUUID(string: uuid.value), value: value?.toData())
+//     }
+// }
 
-extension CBCharacteristic {
-    func toBleCharacteristic() -> BleCharacteristic {
-        return BleCharacteristic(
-            uuid: uuid.toUUID(),
-            properties: [Int64(properties.rawValue)],
-            permissions: [],
-            descriptors: nil,
-            value: value?.toFlutterBytes()
-        )
-    }
-}
+// extension CBCharacteristic {
+//     func toBleCharacteristic() -> BleCharacteristic {
+//         return BleCharacteristic(
+//             uuid: uuid.toUUID(),
+//             properties: [Int64(properties.rawValue)],
+//             permissions: [],
+//             descriptors: nil,
+//             value: value?.toFlutterBytes()
+//         )
+//     }
+// }
 
-extension BleCharacteristic {
-    func toCBChar() -> CBMutableCharacteristic {
-        let properties: [CBCharacteristicProperties] = self.properties.compactMap { int64 in
-            int64?.toCBCharacteristicProperties()
-        }
-        let permissions: [CBAttributePermissions] = self.permissions.compactMap { int64 in
-            int64?.toCBAttributePermissions()
-        }
+// extension BleCharacteristic {
+//     func toCBChar() -> CBMutableCharacteristic {
+//         let properties: [CBCharacteristicProperties] = self.properties.compactMap { int64 in
+//             int64?.toCBCharacteristicProperties()
+//         }
+//         let permissions: [CBAttributePermissions] = self.permissions.compactMap { int64 in
+//             int64?.toCBAttributePermissions()
+//         }
 
-        let combinedProperties = properties.reduce(CBCharacteristicProperties()) { $0.union($1) }
-        let combinedPermissions = permissions.reduce(CBAttributePermissions()) { $0.union($1) }
+//         let combinedProperties = properties.reduce(CBCharacteristicProperties()) { $0.union($1) }
+//         let combinedPermissions = permissions.reduce(CBAttributePermissions()) { $0.union($1) }
 
-        let char = CBMutableCharacteristic(
-            type: CBUUID(string: uuid.value),
-            properties: combinedProperties,
-            value: value?.toData(),
-            permissions: combinedPermissions
-        )
-        char.descriptors = descriptors?.compactMap { desc in
-            desc?.toCBMutableDescriptor()
-        }
-        // Add local refrence of this characteristic
-        let containsChar = characteristicsList.contains { $0.uuid.uuidString == char.uuid.uuidString }
-        if !containsChar { characteristicsList.append(char) }
-        return char
-    }
+//         let char = CBMutableCharacteristic(
+//             type: CBUUID(string: uuid.value),
+//             properties: combinedProperties,
+//             value: value?.toData(),
+//             permissions: combinedPermissions
+//         )
+//         char.descriptors = descriptors?.compactMap { desc in
+//             desc?.toCBMutableDescriptor()
+//         }
+//         // Add local refrence of this characteristic
+//         let containsChar = characteristicsList.contains { $0.uuid.uuidString == char.uuid.uuidString }
+//         if !containsChar { characteristicsList.append(char) }
+//         return char
+//     }
 
-    func find() -> CBMutableCharacteristic? {
-        return characteristicsList.first { ch in
-            ch.uuid.uuidString == self.uuid.value
-        }
-    }
-}
+//     func find() -> CBMutableCharacteristic? {
+//         return characteristicsList.first { ch in
+//             ch.uuid.uuidString == self.uuid.value
+//         }
+//     }
+// }
 
-extension Int64 {
-    func toCBCharacteristicProperties() -> CBCharacteristicProperties? {
-        switch self {
-        case 0:
-            return CBCharacteristicProperties.broadcast
-        case 1:
-            return CBCharacteristicProperties.read
-        case 2:
-            return CBCharacteristicProperties.writeWithoutResponse
-        case 3:
-            return CBCharacteristicProperties.write
-        case 4:
-            return CBCharacteristicProperties.notify
-        case 5:
-            return CBCharacteristicProperties.indicate
-        default:
-            return nil
-        }
-    }
+// extension Int64 {
+//     func toCBCharacteristicProperties() -> CBCharacteristicProperties? {
+//         switch self {
+//         case 0:
+//             return CBCharacteristicProperties.broadcast
+//         case 1:
+//             return CBCharacteristicProperties.read
+//         case 2:
+//             return CBCharacteristicProperties.writeWithoutResponse
+//         case 3:
+//             return CBCharacteristicProperties.write
+//         case 4:
+//             return CBCharacteristicProperties.notify
+//         case 5:
+//             return CBCharacteristicProperties.indicate
+//         default:
+//             return nil
+//         }
+//     }
 
-    func toCBAttributePermissions() -> CBAttributePermissions? {
-        switch self {
-        case 0:
-            return CBAttributePermissions.readable
-        case 1:
-            return CBAttributePermissions.writeable
-        case 2:
-            return CBAttributePermissions.readEncryptionRequired
-        case 3:
-            return CBAttributePermissions.writeEncryptionRequired
-        default:
-            return nil
-        }
-    }
-}
+//     func toCBAttributePermissions() -> CBAttributePermissions? {
+//         switch self {
+//         case 0:
+//             return CBAttributePermissions.readable
+//         case 1:
+//             return CBAttributePermissions.writeable
+//         case 2:
+//             return CBAttributePermissions.readEncryptionRequired
+//         case 3:
+//             return CBAttributePermissions.writeEncryptionRequired
+//         default:
+//             return nil
+//         }
+//     }
+// }
 
-extension CBCentral {
-    func toBleCenral() -> BleCentral {
-        return BleCentral(uuid: UUID(value: identifier.uuidString))
-    }
-}
+// extension CBCentral {
+//     func toBleCenral() -> BleCentral {
+//         return BleCentral(uuid: UUID(value: identifier.uuidString))
+//     }
+// }
 
-extension CBUUID {
-    func toUUID() -> UUID {
-        return UUID(value: uuidString)
-    }
-}
+// extension CBUUID {
+//     func toUUID() -> UUID {
+//         return UUID(value: uuidString)
+//     }
+// }
 
 extension FlutterStandardTypedData {
     func toData() -> Data {
--- ios/Classes/BlePeripheralPlugin.swift
+++ ios/Classes/BlePeripheralPlugin.swift
@@ -5,7 +5,7 @@
     import FlutterMacOS
 #endif
 import Foundation
-import CoreBluetooth
+// import CoreBluetooth
 
 public class BlePeripheralPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -15,17 +15,17 @@ public class BlePeripheralPlugin: NSObject, FlutterPlugin {
         #elseif os(macOS)
             messenger = registrar.messenger
         #endif
-        let bleCallback = BleCallback(binaryMessenger: messenger!)
-        let api = BlePeripheralDarwin(bleCallback: bleCallback)
-        BlePeripheralChannelSetup.setUp(binaryMessenger: messenger!, api: api)
+        // let bleCallback = BleCallback(binaryMessenger: messenger!)
+        // let api = BlePeripheralDarwin(bleCallback: bleCallback)
+        // BlePeripheralChannelSetup.setUp(binaryMessenger: messenger!, api: api)
     }
 }
 
-private class BlePeripheralDarwin: NSObject, BlePeripheralChannel, CBPeripheralManagerDelegate {
+private class BlePeripheralDarwin: NSObject, BlePeripheralChannel {
     
     var bleCallback: BleCallback
-    lazy var peripheralManager: CBPeripheralManager = .init(delegate: self, queue: nil, options: nil)
-    var cbCentrals = [CBCentral]()
+    // lazy var peripheralManager: CBPeripheralManager = .init(delegate: self, queue: nil, options: nil)
+    // var cbCentrals = [CBCentral]()
 
     init(bleCallback: BleCallback) {
         self.bleCallback = bleCallback
@@ -34,22 +34,22 @@ private class BlePeripheralDarwin: NSObject, BlePeripheralChannel, CBPeripheralM
 
     func initialize() throws {
         print("Initialize called")
-        print("isAdvertising \(peripheralManager.isAdvertising)")
+        print("isAdvertising false")
     }
 
     func isSupported() throws -> Bool {
         print("isSupportedCalled")
         // TODO: implement this
-        return true
+        return false
     }
 
     func isAdvertising() throws -> Bool {
-        return peripheralManager.isAdvertising
+        return false
     }
 
     func addServices(services: [BleService]) throws {
         services.forEach { service in
-            self.peripheralManager.add(service.toCBService())
+            print("iOS not implemented")
         }
     }
 
@@ -72,80 +72,22 @@ private class BlePeripheralDarwin: NSObject, BlePeripheralChannel, CBPeripheralM
     }
 
     func stopAdvertising() throws {
-        peripheralManager.stopAdvertising()
-    }
-
-    func updateCentralList(central: CBCentral) {
-        let containsDevice = cbCentrals.contains { $0.identifier == central.identifier }
-        if !containsDevice { cbCentrals.append(central) }
+        print("stopAdvertising called")
     }
 
     func updateCharacteristic(central: BleCentral, characteristic: BleCharacteristic, value: FlutterStandardTypedData) throws {
-        let centralDevice: CBCentral? = cbCentrals.first(where: { device in
-            central.uuid.value == device.identifier.uuidString
-        })
-        let char: CBMutableCharacteristic? = characteristic.find()
-        if centralDevice == nil {
-            throw CustomError.notFound("\(central.uuid.value) device not found")
-        } else if char == nil {
-            throw CustomError.notFound("\(characteristic.uuid.value) characteristic not found")
-        } else {
-            peripheralManager.updateValue(value.toData(), for: char!, onSubscribedCentrals: [centralDevice!])
-        }
-    }
-
-    internal func updateChar(_ inputReport: [Int8], _ characteristic: CBMutableCharacteristic, device: CBCentral) {
-        let data = Data(bytes: inputReport, count: inputReport.count)
-        peripheralManager.updateValue(data, for: characteristic, onSubscribedCentrals: [device])
-    }
-
-    /// Swift callbacks
-    internal nonisolated func peripheralManagerDidStartAdvertising(_: CBPeripheralManager, error: Error?) {
-        bleCallback.onAdvertisingStarted(error: error?.localizedDescription, completion: {})
-    }
-
-    nonisolated func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        print("BluetoothState: \(peripheral.state)")
-        bleCallback.onBleStateChange(state: peripheral.state == .poweredOn, completion: {})
-    }
-
-    internal nonisolated func peripheralManager(_: CBPeripheralManager, didAdd service: CBService, error: Error?) {
-        bleCallback.onServiceAdded(service: service.toBleService(), error: error?.localizedDescription, completion: {})
-    }
-
-    internal nonisolated func peripheralManager(_: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        updateCentralList(central: central)
-        bleCallback.onSubscribe(bleCentral: central.toBleCenral(), characteristic: characteristic.toBleCharacteristic()) {}
-    }
-
-    internal nonisolated func peripheralManager(_: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
-        bleCallback.onUnsubscribe(bleCentral: central.toBleCenral(), characteristic: characteristic.toBleCharacteristic()) {}
-    }
-
-    internal nonisolated func peripheralManager(_: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        bleCallback.onReadRequest(
-            characteristic: request.characteristic.toBleCharacteristic(),
-            offset: Int64(request.offset),
-            value: request.value?.toFlutterBytes()
-        ) { readReq in
-            let data = readReq?.value.toData()
-            if data == nil {
-                self.peripheralManager.respond(to: request, withResult: .requestNotSupported)
-            } else {
-                request.value = data!
-                self.peripheralManager.respond(to: request, withResult: .success)
-            }
-        }
-    }
-
-    internal nonisolated func peripheralManager(_: CBPeripheralManager, didReceiveWrite request: [CBATTRequest]) {
-        request.forEach { req in
-            bleCallback.onWriteRequest(
-                characteristic: req.characteristic.toBleCharacteristic(),
-                offset: Int64(req.offset),
-                value: req.value?.toFlutterBytes()
-            ) {}
-        }
+        print("updateCharacteristic called")
+        // let centralDevice: CBCentral? = cbCentrals.first(where: { device in
+        //     central.uuid.value == device.identifier.uuidString
+        // })
+        // let char: CBMutableCharacteristic? = characteristic.find()
+        // if centralDevice == nil {
+        //     throw CustomError.notFound("\(central.uuid.value) device not found")
+        // } else if char == nil {
+        //     throw CustomError.notFound("\(characteristic.uuid.value) characteristic not found")
+        // } else {
+        //     peripheralManager.updateValue(value.toData(), for: char!, onSubscribedCentrals: [centralDevice!])
+        // }
     }
 }
 
