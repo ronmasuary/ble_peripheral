ae08b1b6768fc82edb954e2324a401ea588f46b1
levischechter <35228498+levischechter@users.noreply.github.com>
2024-08-04 21:14:17 +0300

    remove enable bluetooth on stop advertising

--- android/src/main/kotlin/com/rohit/ble_peripheral/BlePeripheralPlugin.kt
+++ android/src/main/kotlin/com/rohit/ble_peripheral/BlePeripheralPlugin.kt
@@ -147,9 +147,9 @@ class BlePeripheralPlugin : FlutterPlugin, BlePeripheralChannel, ActivityAware,
     }
 
     override fun stopAdvertising() {
-        if (!isBluetoothEnabled()) {
-            enableBluetooth()
-        }
+//        if (!isBluetoothEnabled()) {
+//            enableBluetooth()
+//        }
         handler.post {
             try {
                 bluetoothLeAdvertiser?.stopAdvertising(advertiseCallback)
